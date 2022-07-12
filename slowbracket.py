6d455d5fb91c23ab5394ccb827f9ef4748127d34
Grant Warman <grant.warman@gmail.com>
2022-07-12 17:14:23 -0600

    merge singles and doubles graphs. tweak graph output

--- slowbracket.py
+++ slowbracket.py
@@ -3,9 +3,13 @@ import json
 import networkx as nx
 import matplotlib.pyplot as plt
 import os
-os.add_dll_directory("C:/Program Files/Graphviz/bin")
+import datetime
+os.add_dll_directory('C:/Program Files/Graphviz/bin')
 import pygraphviz
+import itertools
+from collections import defaultdict
 
+# add dubs to graph
 
 # JSON structure 
 
@@ -39,37 +43,70 @@ def set_end(s):
         return s['completedAt']
     return s['createdAt'] 
 
+def remove_empty_sets(sets):
+    return [s for s in sets if s['entrant1Id'] and s['entrant2Id']] # remove empty sets
+
 def build_graph(entrants, sets):
     g = nx.DiGraph()
     g.graph['graph']={'rankdir':'LR'}
-    sets = [s for s in sets if s['entrant1Id'] and s['entrant2Id']] # remove empty sets
-    g.add_nodes_from([s['id'] for s in sets])
+    labels = get_set_labels(entrants, sets)
+    g.add_nodes_from([labels[s['id']] for s in sets])
     sorted_sets = sorted(sets, key=set_end) 
+    players = set([])
+    player2Entrant = defaultdict(set)
     for entrant in entrants:
-        sets = [s for s in sorted_sets if entrant['entrantId'] in set([s['entrant1Id'], s['entrant2Id']])]
-        cur_set = sets[0]
-        for s in sets:
-            if cur_set['id'] == s['id']:
-                continue
-            g.add_edge(cur_set['id'], s['id'], duration=s['completedAt'] - cur_set['completedAt'])
-            cur_set = s
+        for p in entrant['mutations']['players'].values():
+            players.add(p['id'])
+            player2Entrant[p['id']].add(entrant['entrantId'])
+
+    for player in players:
+        sets = [s for s in sorted_sets if not set([s['entrant1Id'], s['entrant2Id']]).isdisjoint(player2Entrant[player])]
+        if sets:
+            cur_set = sets[0]
+            for s in sets:
+                if cur_set['id'] == s['id']:
+                    continue
+                duration = s['completedAt'] - cur_set['completedAt']
+                g.add_edge(labels[cur_set['id']], labels[s['id']], duration=duration, label=str(datetime.timedelta(seconds=duration)))
+                cur_set = s
     return g
+
+def get_entrant_label(players):
+    if len(players) == 0:
+        return ''
+    players_values = list(players.values())
+    if len(players) == 1:
+        return players_values[0]['gamerTag']
+    return players_values[0]['gamerTag'] + ' / ' + players_values[1]['gamerTag']
+
 
 def get_set_labels(entrants, sets):
     labels = {} # set id -> label
     entrant_labels = {} # entrantId -> label
     for entrant in entrants:
-        entrant_labels[entrant['entrantId']] = [players]
+        entrant_labels[entrant['entrantId']] = get_entrant_label(entrant['mutations']['players'])
+    for s in sets:
+        if not s['entrant1Id'] and not s['entrant2Id']:
+            label = ''
+        elif not s['entrant1Id'] or not s['entrant2Id']:
+            label = entrant_labels[s['entrant1Id'] or s['entrant2Id']] + ' bye'
+        else:
+            label = entrant_labels[s['entrant1Id']] + ' vs. ' + entrant_labels[s['entrant2Id']]
+        labels[s['id']] = s['midRoundText'] + ": " + label
+    return labels
 
 
-doubles = read_json_url('https://api.smash.gg/phase_group/1803504?expand[]=sets&expand[]=seeds')
-# singles = read_json_url('https://api.smash.gg/phase_group/1803505?expand[]=sets&expand[]=seeds')
+doubles = read_json_url('https://api.smash.gg/phase_group/1803505?expand[]=sets&expand[]=seeds')
+singles = read_json_url('https://api.smash.gg/phase_group/1803504?expand[]=sets&expand[]=seeds')
 
 doubles_entrants = doubles['entities']['seeds']
-doubles_sets = doubles['entities']['sets']
-doubles_graph = build_graph(doubles_entrants, doubles_sets)
-print(get_set_labels(doubles_entrants, doubles_sets))
+doubles_sets = remove_empty_sets(doubles['entities']['sets'])
+singles_entrants = singles['entities']['seeds']
+singles_sets = remove_empty_sets(singles['entities']['sets'])
+doubles_graph = build_graph(doubles_entrants + singles_entrants, doubles_sets + singles_sets)
+for match in sorted(doubles_graph.edges(data=True), key=lambda e: e[2]['duration'], reverse=True):
+    print(match[2]['label'] + ": " + match[0] + " -> " +  match[1])
 
 a = nx.nx_agraph.to_agraph(doubles_graph)
-a.layout('dot', args='-Nfontsize=10 -Nwidth=".2" -Nheight=".2" -Nmargin=0 -Gfontsize=8 ')
+a.layout('dot', args="-Nfontsize=10 -Nwidth='.5' -Nheight='.5' -Nmargin=0 -Gfontsize=8 -Gnodesep=.6 -Gsplines=true -Gsep='+25,25'")
 a.draw('test.png')
